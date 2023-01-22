8f953b6b9eaa54502d4f6ec47cadde9b6271c1ef
thomas-rooty <essainono1@gmail.com>
2023-01-22 20:57:21 +0100

    added earth

--- components/earth/StartingEarth.tsx
+++ components/earth/StartingEarth.tsx
@@ -1,4 +1,6 @@
 import useSpline from '@splinetool/r3f-spline'
+import {useFrame} from "@react-three/fiber";
+import {useRef} from "react";
 
 interface StartingEarthProps {
   position: [number, number, number]
@@ -8,9 +10,18 @@ interface StartingEarthProps {
 
 const StartingEarth = ({position, rotation, scale}: StartingEarthProps) => {
   const {nodes} = useSpline('https://prod.spline.design/4qGC-5kRLw12RYQp/scene.splinecode')
+
+  // Earth reference
+  const earthRef = useRef<any>()
+
+  // Earth rotation
+  useFrame(({clock}) => {
+    if (!earthRef.current) return
+    earthRef.current.rotation.y = clock.getElapsedTime() / 33
+  })
+
   return (
-    <>
-      <group name="earth" position={position} rotation={rotation} scale={scale}>
+      <group ref={earthRef} name="earth" position={position} rotation={rotation} scale={scale}>
         <mesh name="lands" geometry={nodes.lands.geometry} material={nodes.lands.material} castShadow={true}
               receiveShadow={true}/>
         <mesh
@@ -21,7 +32,6 @@ const StartingEarth = ({position, rotation, scale}: StartingEarthProps) => {
           receiveShadow={true}
         />
       </group>
-    </>
   )
 }
 
--- pages/scene.tsx
+++ pages/scene.tsx
@@ -5,8 +5,8 @@ const Scene = () => {
   return (
     <Canvas>
       <ambientLight intensity={0.1} />
-      <directionalLight color="white" position={[0, 0, 5]} />
-      <StartingEarth position={[1, 0, 0]} rotation={[0, 0, 0]} scale={0.009} />
+      <directionalLight color="white" position={[1, 1.5, 1.5]} />
+      <StartingEarth position={[0, 0, 0]} rotation={[0, 0, 0]} scale={0.033} />
     </Canvas>
   )
 }
