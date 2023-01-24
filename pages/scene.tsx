bfd39a8a44dd97790952db000ffd106afe592b37
thomas-rooty <essainono1@gmail.com>
2023-01-24 18:51:23 +0100

    added ship interiors

--- pages/scene.tsx
+++ pages/scene.tsx
@@ -1,12 +1,24 @@
-import { Canvas } from '@react-three/fiber'
-import StartingEarth from "@/components/earth/StartingEarth";
+import {Canvas} from '@react-three/fiber'
+import StartingEarth from "@/components/StartingCinematic/earth/StartingEarth";
+import ShipInteriors from "@/components/StartingCinematic/ship/Ship";
+import {Stars} from "@react-three/drei";
 
 const Scene = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="white" position={[1, 1.5, 1.5]} />
-      <StartingEarth position={[0, 0, 0]} rotation={[0, 0, 0]} scale={0.033} />
+    <Canvas
+      shadows={true}
+    >
+      <directionalLight
+        color="white"
+        position={[5, 5, 3]}
+        castShadow={false}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <Stars radius={1} depth={25} count={1250} factor={0.5} saturation={1} fade />
+      <StartingEarth position={[0, 0, 2.5]} rotation={[0, 0, 0]} scale={0.01}/>
+      <ShipInteriors position={[-0.495, -0.153, 5]} rotation={[0, 0, 0]} scale={0.001}/>
+
     </Canvas>
   )
 }
