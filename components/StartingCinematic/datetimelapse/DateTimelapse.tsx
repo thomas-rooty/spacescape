5e5853ff069546c0ee3a50999ddee56de09c1dbc
thomas-rooty <essainono1@gmail.com>
2023-01-28 22:02:40 +0100

    remade button + dateTimelapse

--- components/StartingCinematic/datetimelapse/DateTimelapse.tsx
+++ components/StartingCinematic/datetimelapse/DateTimelapse.tsx
@@ -11,22 +11,18 @@ const DateTimelapse = () => {
 
   // The dates are going from 2023 to 2300
   if (startedGame) {
-    // Add styles.yearAnimation to the div with id year
-    document.getElementById('year')?.classList.add(styles.yearAnimation)
     setTimeout(() => {
       if (year < 2300) {
         setYear(year + 1)
       } else {
         setYear(2300)
-        // Remove styles.yearAnimation to the div with id year
-        document.getElementById('year')?.classList.remove(styles.yearAnimation)
       }
     }, 15)
   }
 
   return (
     <div className={styles.dateTimelapse}>
-      <div className={styles.dateText}><b>EARTH - </b><div id='year'>&nbsp;{year}</div></div>
+      <div className={styles.dateText}>A.D.&nbsp;<div id='year'>{year}</div>&nbsp;| EARTH</div>
     </div>
   )
 }
