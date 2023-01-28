14920f13f4397d39a59450cf213e01269ecee39a
thomas-rooty <essainono1@gmail.com>
2023-01-28 23:17:51 +0100

    removed glass

--- components/StartingCinematic/ship/Spaceship.tsx
+++ components/StartingCinematic/ship/Spaceship.tsx
@@ -2,9 +2,6 @@ import * as THREE from "three";
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
-import {useTexture} from "@react-three/drei"
-import {useStore} from "@/utils/zustore";
-import {useFrame} from "@react-three/fiber";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -24,29 +21,7 @@ interface SpaceshipProps {
 
 const Spaceship = ({position, rotation, scale}: SpaceshipProps) => {
   const shipRef = useRef<any>()
-  const glassRef = useRef<any>()
   const { nodes, materials } = useGLTF("/models/spaceship.gltf") as unknown as GLTFResult;
-
-  // Get startedGame from store
-  const startedGame = useStore((state) => state.startedGame)
-
-  // Load textures
-  const [spaceshipTexture] = useTexture([
-    '/models/tex/spaceship/Color_map.png',
-  ])
-
-  // Useful variables
-  let animationDone = false
-
-  // Toggle glass opacity on game start
-  useFrame(() => {
-    if (startedGame && glassRef.current.material.opacity < 0.05) {
-      glassRef.current.material.opacity += 0.0001;
-    } else if (!animationDone && startedGame && glassRef.current.material.opacity >= 0.05) {
-      glassRef.current.material.opacity = 0.05;
-      animationDone = true;
-    }
-  })
 
   return (
     <group ref={shipRef} name="shipinteriors" position={position} rotation={rotation} scale={scale}>
@@ -56,23 +31,7 @@ const Spaceship = ({position, rotation, scale}: SpaceshipProps) => {
         geometry={nodes.Body_ship.geometry}
         material={materials.lambert7}
         rotation={[Math.PI / 2, 0, 0]}
-      >
-        <mesh
-          name="Glasse_ship"
-          ref={glassRef}
-          castShadow={true}
-          receiveShadow={true}
-          geometry={nodes.Glasse_ship.geometry}
-        >
-          <meshLambertMaterial
-            attach="material"
-            map={spaceshipTexture}
-            fog={false}
-            transparent={true}
-            opacity={0}
-          />
-        </mesh>
-      </mesh>
+      />
     </group>
   );
 }
