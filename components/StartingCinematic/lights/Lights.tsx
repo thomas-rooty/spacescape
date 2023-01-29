8c0a510b90cb105497522e70f837281bb23bdf85
thomas-rooty <essainono1@gmail.com>
2023-01-29 12:31:36 +0100

    added a light

--- components/StartingCinematic/lights/Lights.tsx
+++ components/StartingCinematic/lights/Lights.tsx
@@ -4,18 +4,21 @@ import {useRef} from "react";
 
 type LightsPosition = {
   light1: [number, number, number];
+  light2: [number, number, number];
 }
 
 const Lights = () => {
   const LightsPosition: LightsPosition = {
-    light1: [-0.98, -1, -0.18]
+    light1: [-0.98, -1, -0.18],
+    light2: [0, 5, 25],
   }
   // Get startedGame, animationDone from store
   const startedGame = useStore((state) => state.startedGame)
   const animationDone = useStore((state) => state.animationDone)
 
   // Light ref
-  const lightRef = useRef<any>()
+  const light1Ref = useRef<any>()
+  const light2Ref = useRef<any>()
 
   // Useful variables
   let animationAliveTime = 0
@@ -28,9 +31,9 @@ const Lights = () => {
   useFrame(() => {
     if (startedGame && !animationDone) {
       animationAliveTime += 0.01 * 3.14 / 10
-      lightRef.current.position.x = Math.sin((27 * 3.14) * Math.cos(animationAliveTime) + 14)
-      lightRef.current.position.y = Math.cos((27 * 3.14) * Math.cos(animationAliveTime))
-      lightRef.current.position.z = Math.cos((27 * 3.14) * Math.cos(animationAliveTime) + 14)
+      light1Ref.current.position.x = Math.sin((27 * 3.14) * Math.cos(animationAliveTime) + 14)
+      light1Ref.current.position.y = Math.cos((27 * 3.14) * Math.cos(animationAliveTime))
+      light1Ref.current.position.z = Math.cos((27 * 3.14) * Math.cos(animationAliveTime) + 14)
     }
   })
 
@@ -38,8 +41,18 @@ const Lights = () => {
     <>
       <directionalLight
         color="#ffffcc"
-        ref={lightRef}
+        ref={light1Ref}
         position={LightsPosition.light1}
+        castShadow={false}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <spotLight
+        color="#ffffcc"
+        ref={light2Ref}
+        position={LightsPosition.light2}
+        angle={2}
+        intensity={0.2}
         castShadow={false}
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
@@ -47,6 +60,10 @@ const Lights = () => {
       <ambientLight intensity={0.1}/>
       {/* These are boxes that are used to visualize the light sources */}
       <mesh position={LightsPosition.light1}>
+        <boxBufferGeometry args={[0.5, 0.5, 0.5]}/>
+        <meshStandardMaterial color="hotpink"/>
+      </mesh>
+      <mesh position={LightsPosition.light2}>
         <boxBufferGeometry args={[0.5, 0.5, 0.5]}/>
         <meshStandardMaterial color="hotpink"/>
       </mesh>
