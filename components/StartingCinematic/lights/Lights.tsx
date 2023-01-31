9a3d80a89168a6d60c6bdda0888c97cdc92dd290
thomas-rooty <essainono1@gmail.com>
2023-01-31 23:17:59 +0100

    edited light angle

--- components/StartingCinematic/lights/Lights.tsx
+++ components/StartingCinematic/lights/Lights.tsx
@@ -38,7 +38,7 @@ const Lights = () => {
     <>
       {/* These are the lights that are used to light up the scene */}
       <Spot position={LightsPosition.light1} target={[0, 0, -6]} color="white" penumbra={0} distance={100} angle={90} attenuation={1} anglePower={5} intensity={2} />
-      <Spot position={LightsPosition.light2} target={[0, 0, 25 + 0.09]} color="#f0f2ff" penumbra={0} distance={5} angle={90} attenuation={1} anglePower={5} intensity={2} />
+      <Spot position={LightsPosition.light2} target={[0, 0, 25 + 0.09]} color="#f0f2ff" penumbra={1} distance={5} angle={3} attenuation={1} anglePower={0} intensity={2} />
       <ambientLight intensity={0.1} />
       {/* These are boxes that are used to visualize the light sources */}
       <mesh position={LightsPosition.light1}>
