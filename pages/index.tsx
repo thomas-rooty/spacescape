103da35c81c0e136d31bcda334db66156cb06f8a
thomas-rooty <essainono1@gmail.com>
2023-01-31 23:26:57 +0100

    improved loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,33 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Scene from '@/pages/scene'
 import DateTimelapse from '@/components/StartingCinematic/datetimelapse/DateTimelapse'
+import {Suspense} from 'react'
+import Loader from "@/components/loader/Loader";
+import StartBtn from "@/components/StartingCinematic/buttons/StartBtn";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>SpaceScape</title>
-        <meta name="description" content="🚀 Jacob stranded on an anonymous planet, gather resources to survive and connect with other survivors!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:description" content="🚀 Jacob stranded on an anonymous planet, gather resources to survive and connect with other survivors!" />
-        <meta property="og:title" content="SpaceScape" />
-        <meta property="og:image" content="/img/spacescape_banner.png" />
-        <meta itemProp="image" content="/img/spacescape_banner.png" />
-        <meta name="twitter:image" content="/img/spacescape_banner.png" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description"
+              content="🚀 Jacob stranded on an anonymous planet, gather resources to survive and connect with other survivors!"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta property="og:description"
+              content="🚀 Jacob stranded on an anonymous planet, gather resources to survive and connect with other survivors!"/>
+        <meta property="og:title" content="SpaceScape"/>
+        <meta property="og:image" content="/img/spacescape_banner.png"/>
+        <meta itemProp="image" content="/img/spacescape_banner.png"/>
+        <meta name="twitter:image" content="/img/spacescape_banner.png"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <div className={styles.scene}>
-        <Scene />
-        <DateTimelapse />
-      </div>
+      <Suspense fallback={<Loader/>}>
+        <div className={styles.scene}>
+          <Scene/>
+          <DateTimelapse/>
+          <StartBtn/>
+        </div>
+      </Suspense>
     </>
   )
 }
--- pages/scene.tsx
+++ pages/scene.tsx
@@ -1,16 +1,13 @@
-import { Canvas } from '@react-three/fiber'
-import { Stars, PointerLockControls } from '@react-three/drei'
-import { Physics, Debug } from '@react-three/cannon'
-import { Suspense } from 'react'
-import { useStore } from '@/utils/zustore'
+import {Canvas} from '@react-three/fiber'
+import {Stars, PointerLockControls} from '@react-three/drei'
+import {Physics, Debug} from '@react-three/cannon'
+import {useStore} from '@/utils/zustore'
 import DyingEarth from '@/components/StartingCinematic/earth/DyingEarth'
 import Lights from '@/components/StartingCinematic/lights/Lights'
 import Effects from '@/components/effects/Effects'
 import Floor from '@/components/StartingCinematic/floor/Floor'
 import BaseCharacter from '@/components/character/BaseCharacter'
 import CockpitCollision from '@/components/StartingCinematic/ship/CockpitCollision'
-import StartBtn from '@/components/StartingCinematic/buttons/StartBtn'
-import Loader from '@/components/loader/Loader'
 import Spaceship from "@/components/StartingCinematic/ship/Spaceship";
 
 const Scene = () => {
@@ -22,24 +19,21 @@ const Scene = () => {
 
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Canvas shadows={true} camera={{ position: [0, 0, distanceFromCenter], fov: 40 }}>
-          <Effects />
-          <Lights />
-          <Physics gravity={[0, -9.8, 0]}>
-            <BaseCharacter position={[0, 0, distanceFromCenter + 0.09]} args={[0.14]} />
-            <Debug scale={0} color="red">
-              <Floor rotation={[Math.PI / -2, 0, 0]} color={'black'} position={[-0.33, -0.17, distanceFromCenter + 0.2]} />
-              <CockpitCollision />
-            </Debug>
-          </Physics>
-          {animationDone && <PointerLockControls />}
-          <Stars radius={1} depth={25} count={2500} factor={0.5} saturation={1} fade />
-          <DyingEarth position={[0, 0, -6]} rotation={[0, 0, 0]} scale={9.33} />
-          <Spaceship position={[0, -3.75, 27]} rotation={[0, 0, 0]} scale={0.006} />
-        </Canvas>
-        <StartBtn />
-      </Suspense>
+      <Canvas shadows={true} camera={{position: [0, 0, distanceFromCenter], fov: 40}}>
+        <Effects/>
+        <Lights/>
+        <Physics gravity={[0, -9.8, 0]}>
+          <BaseCharacter position={[0, 0, distanceFromCenter + 0.09]} args={[0.14]}/>
+          <Debug scale={0} color="red">
+            <Floor rotation={[Math.PI / -2, 0, 0]} color={'black'} position={[-0.33, -0.17, distanceFromCenter + 0.2]}/>
+            <CockpitCollision/>
+          </Debug>
+        </Physics>
+        {animationDone && <PointerLockControls/>}
+        <Stars radius={1} depth={25} count={2500} factor={0.5} saturation={1} fade/>
+        <DyingEarth position={[0, 0, -6]} rotation={[0, 0, 0]} scale={9.33}/>
+        <Spaceship position={[0, -3.75, 27]} rotation={[0, 0, 0]} scale={0.006}/>
+      </Canvas>
     </>
   )
 }
