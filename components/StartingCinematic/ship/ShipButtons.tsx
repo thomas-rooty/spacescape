c3deb1ed73f41ab09d49b7678cbe787003728723
thomas-rooty <essainono1@gmail.com>
2023-02-01 16:25:28 +0100

    added SHIP_BTN_START

--- components/StartingCinematic/ship/ShipButtons.tsx
+++ components/StartingCinematic/ship/ShipButtons.tsx
@@ -9,26 +9,43 @@ const ShipButtons = () => {
   const hoveredObject = useStore((state) => state.hoveredObject)
 
   // Create refs for the buttons
-  const SHIP_SELECT_BTN = useRef<any>()
+  const SHIP_BTN_SELECT = useRef<any>()
+  const SHIP_BTN_START = useRef<any>()
   const {interact} = useControls()
 
   useFrame(() => {
-    // Add SHIP_SELECT_BTN as a hoverable object if not already in the list
-    if (SHIP_SELECT_BTN.current && SHIP_SELECT_BTN.current.uuid) {
-      addObjectAsHoverable(SHIP_SELECT_BTN.current)
+    // Add SHIP_BTN_SELECT as a hoverable object
+    if (SHIP_BTN_SELECT.current && SHIP_BTN_SELECT.current.uuid) {
+      addObjectAsHoverable(SHIP_BTN_SELECT.current)
     }
 
-    // Change the color of the SHIP_SELECT_BTN object if it is hovered
+    // Change the color of the SHIP_BTN_SELECT object if it is hovered
     if (hoveredObject === 'SHIP_SELECT_BTN' && interact) {
-      SHIP_SELECT_BTN.current.material.color.set('red')
+      SHIP_BTN_SELECT.current.material.color.set('red')
     } else {
-      SHIP_SELECT_BTN.current.material.color.set('hotpink')
+      SHIP_BTN_SELECT.current.material.color.set('hotpink')
+    }
+
+    // Add SHIP_BTN_START as a hoverable object
+    if (SHIP_BTN_START.current && SHIP_BTN_START.current.uuid) {
+      addObjectAsHoverable(SHIP_BTN_START.current)
+    }
+
+    // Change the color of the SHIP_BTN_START object if it is hovered
+    if (hoveredObject === 'SHIP_START_BTN' && interact) {
+      SHIP_BTN_START.current.material.color.set('red')
+    } else {
+      SHIP_BTN_START.current.material.color.set('hotpink')
     }
   })
 
   return (
     <>
-      <mesh ref={SHIP_SELECT_BTN} userData={{id: 'SHIP_SELECT_BTN'}} position={[-0.68, 0.07, 24.3]}>
+      <mesh ref={SHIP_BTN_SELECT} userData={{id: 'SHIP_SELECT_BTN'}} position={[-0.68, 0.08, 24.3]}>
+        <boxBufferGeometry args={[0.07, 0.07, 0.07]}/>
+        <meshLambertMaterial opacity={0.5} transparent={true}/>
+      </mesh>
+      <mesh ref={SHIP_BTN_START} userData={{id: 'SHIP_START_BTN'}} position={[-0.87, 0.08, 24.41]}>
         <boxBufferGeometry args={[0.07, 0.07, 0.07]}/>
         <meshLambertMaterial opacity={0.5} transparent={true}/>
       </mesh>
