d15eac72ae1262f1a8526dc5e351be15bf9a4f2e
thomas-rooty <essainono1@gmail.com>
2023-02-01 16:37:38 +0100

    added mouse controls

--- utils/useControls.tsx
+++ utils/useControls.tsx
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 
 export const useControls = () => {
   const keys = { KeyW: 'forward', KeyS: 'backward', KeyA: 'left', KeyD: 'right', Space: 'jump', KeyE: 'interact' }
+  const buttons = { 0: 'leftClick', 2: 'rightClick' }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const moveFieldByKey = (key: string | number) => keys[key as keyof typeof keys]
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const moveFieldByButton = (button: number) => buttons[button as keyof typeof buttons]
 
-  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false, interact: false })
+  const [movement, setMovement] = useState({ forward: false, backward: false, left: false, right: false, jump: false, interact: false, leftClick: false, rightClick: false })
 
   useEffect(() => {
     const handleKeyDown = (e: { code: string | number }) =>
@@ -18,15 +21,29 @@ export const useControls = () => {
         ...m,
         [moveFieldByKey(e.code)]: false,
       }))
+    const handleMouseDown = (e: { button: number }) =>
+      setMovement((m) => ({
+        ...m,
+        [moveFieldByButton(e.button)]: true,
+      }))
+    const handleMouseUp = (e: { button: number }) =>
+      setMovement((m) => ({
+        ...m,
+        [moveFieldByButton(e.button)]: false,
+      }))
 
     document.addEventListener('keydown', handleKeyDown)
     document.addEventListener('keyup', handleKeyUp)
+    document.addEventListener('mousedown', handleMouseDown)
+    document.addEventListener('mouseup', handleMouseUp)
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown)
       document.removeEventListener('keyup', handleKeyUp)
+      document.removeEventListener('mousedown', handleMouseDown)
+      document.removeEventListener('mouseup', handleMouseUp)
     }
-  }, [moveFieldByKey])
+  }, [moveFieldByKey, moveFieldByButton])
 
   return movement
 }
