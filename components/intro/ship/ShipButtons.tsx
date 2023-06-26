e2f6376c209dddab536a29790fe691d32a1b79ab
thomas-rooty <tc.caron91@gmail.com>
2023-06-26 21:22:37 +0200

    Conditional buttons

--- components/intro/ship/ShipButtons.tsx
+++ components/intro/ship/ShipButtons.tsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react'
 import ShipButton from './buttons/ShipButton'
+import { createCinematicSlice } from '@/utils/stores/storeIntro'
 
 const ShipButtons = () => {
   const SHIP_BTN_START = useRef()
@@ -7,6 +8,7 @@ const ShipButtons = () => {
   const SHIP_BTN_SELECT = useRef()
   const SHIP_BTN_CHECK = useRef()
   const SHIP_CMD_STEERING = useRef()
+  const endCryo = createCinematicSlice((state) => state.endCryo)
 
   return (
     <>
@@ -31,20 +33,23 @@ const ShipButtons = () => {
         rotation={[0.1, 0.7, -0.07]}
         geometryArgs={[0.09, 0.04, 0]}
       />
-      <ShipButton
-        refProp={SHIP_BTN_CHECK}
-        id='SHIP_BTN_CHECK'
-        position={[0.68, 0.073, 24.3]}
-        rotation={[0.1, -0.7, 0.07]}
-        geometryArgs={[0.09, 0.04, 0.01]}
-      />
-      <ShipButton
-        refProp={SHIP_CMD_STEERING}
-        id='SHIP_CMD_STEERING'
-        position={[0, -0.45, 24.65]}
-        rotation={[-0.5, 0, 0]}
-        geometryArgs={[0.1, 0.1, 0.04]}
-      />
+      {!endCryo ?
+        <ShipButton
+          refProp={SHIP_CMD_STEERING}
+          id='SHIP_CMD_STEERING'
+          position={[0, -0.45, 24.65]}
+          rotation={[-0.5, 0, 0]}
+          geometryArgs={[0.1, 0.1, 0.04]}
+        />
+        :
+        <ShipButton
+          refProp={SHIP_BTN_CHECK}
+          id='SHIP_BTN_CHECK'
+          position={[0.68, 0.073, 24.3]}
+          rotation={[0.1, -0.7, 0.07]}
+          geometryArgs={[0.09, 0.04, 0.01]}
+        />
+      }
     </>
   )
 }
