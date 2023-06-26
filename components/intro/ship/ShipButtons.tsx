835417d10be9bc1e92028d7c38855498a48288b1
thomas-rooty <tc.caron91@gmail.com>
2023-06-26 21:18:59 +0200

    Better ship buttons component

--- components/intro/ship/ShipButtons.tsx
+++ components/intro/ship/ShipButtons.tsx
@@ -1,53 +1,5 @@
-import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
-import { createCinematicSlice } from '@/utils/stores/storeIntro'
-import { useControls } from '@/utils/useControls'
-
-interface ShipButtonProps {
-  refProp: any
-  id: string
-  position: [number, number, number]
-  rotation: [number, number, number]
-  geometryArgs: [number, number, number]
-}
-
-const ShipButton = ({ refProp, id, position, rotation, geometryArgs }: ShipButtonProps) => {
-  const addObjectAsHoverable = createCinematicSlice((state) => state.addObjectAsHoverable)
-  const hoveredObject = createCinematicSlice((state) => state.hoveredObject)
-  const setAudioState = createCinematicSlice((state) => state.setAudioState)
-  const setLaunchInitiated = createCinematicSlice((state) => state.setLaunchInitiated)
-  const setCheckInitiated = createCinematicSlice((state) => state.setCheckInitiated)
-  const { interact } = useControls()
-
-  useFrame(() => {
-    if (refProp.current && refProp.current.uuid) {
-      addObjectAsHoverable(refProp.current)
-    }
-
-    if (hoveredObject === id && interact) {
-      refProp.current.material.color.set('orange')
-
-      if (id === 'SHIP_BTN_START') {
-        setAudioState(true)
-      } else if (id === 'SHIP_BTN_STOP') {
-        setAudioState(false)
-      } else if (id === 'SHIP_BTN_CHECK') {
-        setCheckInitiated(true)
-      } else if (id === 'SHIP_CMD_STEERING') {
-        setLaunchInitiated(true)
-      }
-    } else {
-      refProp.current.material.color.set('#FFD580')
-    }
-  })
-
-  return (
-    <mesh ref={refProp} userData={{ id }} position={position} rotation={rotation}>
-      <boxGeometry args={geometryArgs} />
-      <meshLambertMaterial opacity={0.3} transparent={true} />
-    </mesh>
-  )
-}
+import ShipButton from './buttons/ShipButton'
 
 const ShipButtons = () => {
   const SHIP_BTN_START = useRef()
