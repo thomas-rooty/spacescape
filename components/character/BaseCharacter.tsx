fc7148c0f40a557616d6b15801230f898fed2173
thomas.caron <tc.caron91@gmail.com>
2023-09-07 15:49:57 +0200

    Creating vectors outside of render function and using useMemo

--- components/character/BaseCharacter.tsx
+++ components/character/BaseCharacter.tsx
@@ -7,43 +7,41 @@ import { createCharacterSlice } from '@/utils/stores/character.store'
 import * as THREE from 'three'
 
 const BaseCharacter = (props: SphereProps) => {
-  const direction = new THREE.Vector3()
-  const frontVector = new THREE.Vector3()
-  const sideVector = new THREE.Vector3()
-  const speed = new THREE.Vector3()
-  const SPEED = 0.5
+  const direction = useMemo(() => new THREE.Vector3(), [])
+  const frontVector = useMemo(() => new THREE.Vector3(), [])
+  const sideVector = useMemo(() => new THREE.Vector3(), [])
+  const speed = useMemo(() => new THREE.Vector3(), [])
+  const worldDirection = useMemo(() => new THREE.Vector3(), [])
 
+  const SPEED = 0.5
   const { camera } = useThree()
 
-  // Character collision sphere
   const [ref, api] = useSphere<any>(() => ({
     mass: 1,
     type: 'Dynamic',
-
     position: [0, 10, 0],
     ...props,
   }))
 
-  // Character movement
   const setPosition = createCharacterSlice((state) => state.setPosition)
   const { forward, backward, left, right, jump } = useControls()
   const velocity = useRef<any>([0, 0, 0])
 
-
-  // Subscribe to the velocity of the character
   useEffect(() => api.velocity.subscribe((v) => (velocity.current = v)), [api.velocity])
+  useEffect(() => api.position.subscribe((p) => setPosition({
+    x: p[0],
+    y: p[1],
+    z: p[2],
+  })), [api.position, setPosition])
 
-  // Subscribe to the position of the character and store it in the store
-  useEffect(() => api.position.subscribe((p) => setPosition({ x: p[0], y: p[1], z: p[2] })), [api.position, setPosition])
-
-  // Raycast initialization, gathering of the functions and variables from the store needed to perform hoverable objects detection
   const raycaster = useMemo(() => new THREE.Raycaster(), [])
   const hoverableObjects = createCinematicSlice((state) => state.hoverableObjects)
   const setObjectAsHovered = createCinematicSlice((state) => state.setObjectAsHovered)
   const shaking = createCharacterSlice((state) => state.shaking)
 
   useFrame(({ clock }) => {
-    // Movement system gestion
+    const elapsedTime = clock.getElapsedTime()
+
     ref.current.getWorldPosition(camera.position)
     frontVector.set(0, 0, Number(backward) - Number(forward))
     sideVector.set(Number(left) - Number(right), 0, 0)
@@ -53,22 +51,19 @@ const BaseCharacter = (props: SphereProps) => {
     api.velocity.set(direction.x, velocity.current[1], direction.z)
     if (jump && Math.abs(velocity.current[1].toFixed(3)) < 0.001) api.velocity.set(velocity.current[0], 1.07, velocity.current[2])
 
-    // Head bobbing effect according to the player movement behavior
     if (direction.x !== 0 || direction.z !== 0) {
-      camera.position.y += Math.sin(clock.getElapsedTime() * 20) / 250
+      camera.position.y += Math.sin(elapsedTime * 20) / 250
     } else {
-      camera.position.y += Math.sin(clock.getElapsedTime() * 2) / 1000
+      camera.position.y += Math.sin(elapsedTime * 2) / 1000
     }
 
-    // Shake camera if shaking is true
     if (shaking) {
-      camera.position.x += Math.sin(clock.getElapsedTime() * 200) / 250
-      camera.position.y += Math.sin(clock.getElapsedTime() * 100) / 250
-      camera.position.z += Math.sin(clock.getElapsedTime() * 200) / 250
+      camera.position.x += Math.sin(elapsedTime * 200) / 250
+      camera.position.y += Math.sin(elapsedTime * 100) / 250
+      camera.position.z += Math.sin(elapsedTime * 200) / 250
     }
 
-    // Raycast detection system
-    raycaster.set(camera.position, camera.getWorldDirection(new THREE.Vector3()))
+    raycaster.set(camera.position, camera.getWorldDirection(worldDirection))
     const intersects = raycaster.intersectObjects(hoverableObjects && Object.keys(hoverableObjects).length > 0 ? hoverableObjects : [])
     if (intersects.length > 0 && intersects[0].distance < 1.3) {
       setObjectAsHovered(intersects[0].object.userData.id)
@@ -81,7 +76,7 @@ const BaseCharacter = (props: SphereProps) => {
     <group>
       <mesh castShadow={true} position={props.position} ref={ref}>
         <sphereGeometry args={props.args} />
-        <meshStandardMaterial color="#FFFF00" />
+        <meshStandardMaterial color='#FFFF00' />
       </mesh>
     </group>
   )
