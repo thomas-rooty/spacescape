5892cf9b5cdb648e041546c6a4d1a04351e803b9
thomas-rooty <tc.caron91@gmail.com>
2024-01-09 21:20:32 +0100

    Stylized landing page as choubibou said !

--- components/intro/IntroExperience.tsx
+++ components/intro/IntroExperience.tsx
@@ -1,22 +1,50 @@
+import styles from '@/styles/Home.module.css'
 import IntroScene from '@/components/intro/scene/IntroScene'
 import DateTimelapse from '@/components/intro/scene/datetimelapse/DateTimelapse'
-import StartBtn from '@/components/intro/scene/buttons/StartBtn'
 import { createCinematicSlice } from '@/utils/stores/intro.store'
+import { useEffect, useState } from 'react'
 
 const IntroExperience = () => {
   // Store values
   const endCryo = createCinematicSlice((state) => state.endCryo)
+  const [showPointerLocker, setShowPointerLocker] = useState(true)
+  const setStartedGame = createCinematicSlice((state) => state.setStartedGame)
+
+  const hideStartBtn = () => {
+    // Hide start button, show pointer locker, and set startedGame to true
+    const startBtn = document.getElementById('startBtn')
+    if (startBtn) {
+      startBtn.classList.add(styles.hide)
+      setTimeout(() => {
+        startBtn.remove()
+      }, 1000)
+    }
+    setStartedGame(true)
+  }
+
+  // Used to handle user that exit pointer lock mode by pressing ESC and not lock them out
+  const pointerLockChange = () => {
+    setShowPointerLocker(!showPointerLocker)
+  }
+
+  useEffect(() => {
+    document.addEventListener('pointerlockchange', pointerLockChange, false)
+    return () => {
+      document.removeEventListener('pointerlockchange', pointerLockChange, false)
+    }
+  })
 
   return (
-    <>
+    <div className={styles.scenes}>
       <IntroScene />
-      {!endCryo && (
-        <>
-          <DateTimelapse />
-          <StartBtn />
-        </>
-      )}
-    </>
+      <div id="startBtn" className={`${styles.welcome} ${showPointerLocker ? styles.show : styles.hide}`}>
+        <h1 className={styles.title} onClick={hideStartBtn}>
+          EXPLORE
+        </h1>
+        <h3 className={styles.subtitle}>a new world</h3>
+      </div>
+      {!endCryo && <DateTimelapse />}
+    </div>
   )
 }
 
