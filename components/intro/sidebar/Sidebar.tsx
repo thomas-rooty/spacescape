5fbf705bf7ab132b4a72370604b3c9e966228e9a
thomas-rooty <tc.caron91@gmail.com>
2024-01-10 20:36:27 +0100

    Better loader

--- components/intro/sidebar/Sidebar.tsx
+++ components/intro/sidebar/Sidebar.tsx
@@ -1,33 +1,34 @@
 import styles from '@/styles/Sidebar.module.css'
 
-const Sidebar = ({showSidebar}: { showSidebar: boolean }) => {
-    return (
-        <div className={`${styles.sidebar} ${showSidebar ? styles.show : styles.hide}`}>
-            <h1 className={styles.title}>Spacescape <span className={styles.subtitle}>- a web experience</span></h1>
-            <div className={styles.synopsis}>
-                <h2 className={styles.synopsisTitle}>- What is Spacescape?</h2>
-                <hr />
-                <p>In the year 2300, Earth has succumbed to an environmental crisis, compelling humanity to venture into
-                    the cosmos in search of a new sanctuary. Welcome to Spacescape, a multiplayer survival adventure
-                    where you are not just a spectator, but an active participant in this interstellar
-                    odyssey.<br/><br/>
-
-                    As a skilled astronaut, you find yourself stranded on a mysterious planet, a world away from the now
-                    uninhabitable Earth. Your spacecraft, once a beacon of hope, lies in ruins, challenging you to
-                    summon your inner resilience. Survival is your primary goal.<br/><br/>
-
-                    In Spacescape, every decision you make has profound implications. You must scavenge for
-                    resources, harnessing the alien environment to sustain life. The elements here are unlike anything
-                    known to humanity, demanding innovative strategies for water procurement, food cultivation, and
-                    energy generation.<br/><br/>
-
-                    This planet is teeming with enigmas and dangers, including unknown entities that lurk in the
-                    shadows. Your survival skills will be tested against these alien creatures, and at times, even
-                    against other survivors. Will you forge alliances or view every encounter as a potential threat?
-                </p>
-            </div>
-        </div>
-    )
+const Sidebar = ({ showSidebar }: { showSidebar: boolean }) => {
+  return (
+    <div className={`${styles.sidebar} ${showSidebar ? styles.show : styles.hide}`}>
+      <h1 className={styles.title}>
+        Spacescape
+        <span className={styles.subtitle}> a web experience</span>
+      </h1>
+      <div className={styles.synopsis}>
+        <h2 className={styles.synopsisTitle}>- What is Spacescape?</h2>
+        <hr />
+        <p>
+          In the year 2300, Earth has succumbed to an environmental crisis, compelling humanity to venture into the cosmos in search of a new sanctuary. Welcome to Spacescape, a multiplayer survival
+          adventure where you are not just a spectator, but an active participant in this interstellar odyssey.
+          <br />
+          <br />
+          As a skilled astronaut, you find yourself stranded on a mysterious planet, a world away from the now uninhabitable Earth. Your spacecraft, once a beacon of hope, lies in ruins, challenging
+          you to summon your inner resilience. Survival is your primary goal.
+          <br />
+          <br />
+          In Spacescape, every decision you make has profound implications. You must scavenge for resources, harnessing the alien environment to sustain life. The elements here are unlike anything
+          known to humanity, demanding innovative strategies for water procurement, food cultivation, and energy generation.
+          <br />
+          <br />
+          This planet is teeming with enigmas and dangers, including unknown entities that lurk in the shadows. Your survival skills will be tested against these alien creatures, and at times, even
+          against other survivors. Will you forge alliances or view every encounter as a potential threat?
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
--- components/loader/Loader.tsx
+++ components/loader/Loader.tsx
@@ -1,23 +1,27 @@
 import styles from '@/styles/Loader.module.css'
-import {useProgress} from "@react-three/drei";
+import { useProgress } from '@react-three/drei'
 
 const Loader = () => {
-  const {progress, loaded, total} = useProgress();
+  const { progress, loaded } = useProgress()
   return (
     <div className={styles.loader}>
       <svg viewBox="0 0 160 160" width="160" height="160">
-        <circle cx="80" cy="80" r="50" fill="#757575"/>
+        <circle cx="80" cy="80" r="50" fill="#757575" />
         <g transform=" matrix(0.866, -0.5, 0.25, 0.433, 80, 80)">
           <path d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z" fill="#fff">
-            <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="1s"
-                              repeatCount="indefinite"/>
+            <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="1s" repeatCount="indefinite" />
           </path>
         </g>
-        <path d="M 50,0 A 50,50 0 0,0 -50,0Z" transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)" fill="#757575"/>
+        <path d="M 50,0 A 50,50 0 0,0 -50,0Z" transform="matrix(0.866, -0.5, 0.5, 0.866, 80, 80)" fill="#757575" />
       </svg>
       <div className={styles.loaderText}>
         <p>{Math.round(progress)}%</p>
-        <p>{loaded} / {total} assets</p>
+        <p>
+          {loaded} assets loaded
+        </p>
+        <div className={styles.progressBarContainer}>
+          <div className={styles.progressBar} style={{ width: `${Math.round(progress)}%` }}></div>
+        </div>
       </div>
     </div>
   )
