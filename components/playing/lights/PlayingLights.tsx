86aa998d68ea93eaadc71e67ffcc9dc0c69a580f
Thomas Caron <tc.caron91@gmail.com>
2024-01-11 17:26:34 +0100

    Add fog

--- components/playing/lights/PlayingLights.tsx
+++ components/playing/lights/PlayingLights.tsx
@@ -31,7 +31,7 @@ const PlayingLights = () => {
     <>
       {/* Ambient light and fog */}
       <ambientLight intensity={0.1} />
-      <fog attach="fog" args={['black', 0, 10000000000]} />
+      <fog attach="fog" args={['black', 0, 5]} />
       {/* Sun */}
       <Spot position={[0, 10, 25]} castShadow={true} target={[0, 0, 25]} color={'#cceaff'} penumbra={1} distance={100} angle={360} attenuation={0} anglePower={0} intensity={2} />
     </>
