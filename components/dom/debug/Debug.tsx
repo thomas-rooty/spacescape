b6c166fa30ad5e713f11b63e7e4d8f0165339334
thomas-rooty <tc.caron91@gmail.com>
2024-01-14 22:45:25 +0100

    Logging Socket ID

--- components/dom/debug/Debug.tsx
+++ components/dom/debug/Debug.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/Debug.module.css'
 import { createDebugStore } from '@/stores/debug.store'
+import { createSocketSlice } from '@/stores/socket.store'
 import { Html } from '@react-three/drei'
 
 // This component is heavy, it should be used only in development and in case of need solely !
@@ -9,15 +10,20 @@ interface DebugProps {
 
 export const Debug = ({ debug }: DebugProps) => {
   const position = createDebugStore((state) => state.position)
+  const socket = createSocketSlice((state) => state.socket)
   return (
     <>
       {debug && (
         <Html>
           <div className={styles.debug}>
             <div className={styles.debug__item}>
+              <h4>Socket ID:</h4>
+              <span>{socket?.id || 'Not connected'}</span>
+            </div>
+            <div className={styles.debug__item}>
               <h4>Position:</h4>
-              <span>x:{position.x.toFixed(2)}</span>
-              <span>y:{position.y.toFixed(2)}</span>
+              <span className={styles.separator}>x:{position.x.toFixed(2)}</span>
+              <span className={styles.separator}>y:{position.y.toFixed(2)}</span>
               <span>z:{position.z.toFixed(2)}</span>
             </div>
           </div>
