e48984554c20567d9810e924e389bb3142c966c4
thomas-rooty <tc.caron91@gmail.com>
2024-01-17 17:06:48 +0100

    Restored playing lights

--- components/scenes/playing/lights/PlayingLights.tsx
+++ components/scenes/playing/lights/PlayingLights.tsx
@@ -3,8 +3,8 @@ import { SpotLight } from '@/components/scenes/common/lights/SpotLight'
 const PlayingLights = () => {
   return (
     <>
-      <ambientLight intensity={1} />
-      <fog attach="fog" args={['black', 0, 12000]} />
+      <ambientLight intensity={0.2} />
+      <fog attach="fog" args={['black', 0, 20]} />
       <SpotLight position={[-10, 15, -15]} target={[6, 0, 28]} angle={Math.PI/4} intensity={1.2} penumbra={1} distance={0} bias={-0.000001} />
     </>
   )
