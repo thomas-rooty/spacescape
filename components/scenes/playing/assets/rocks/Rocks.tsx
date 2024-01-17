f429ee4f4f039b4640a7d1302d8e4e97848863f2
thomas-rooty <tc.caron91@gmail.com>
2024-01-18 00:16:21 +0100

    Removed gold as it was too expensive !

--- components/scenes/playing/assets/rocks/Rocks.tsx
+++ components/scenes/playing/assets/rocks/Rocks.tsx
@@ -45,7 +45,7 @@ const Rocks = ({ count = 1000 }: StoneCountProps) => {
 
   return (
     <InstancedRigidBodies ref={rigidBodies} instances={instances} colliders="cuboid" type="fixed" onIntersectionEnter={(e) => console.log(e)}>
-      <instancedMesh castShadow args={[nodes.Rock_3.geometry, undefined, count]} count={count} >
+      <instancedMesh castShadow args={[nodes.Rock_3.geometry, undefined, count]} count={count}>
         <meshStandardMaterial attach="material" {...materials.Stone_Dark} />
       </instancedMesh>
     </InstancedRigidBodies>
@@ -53,49 +53,68 @@ const Rocks = ({ count = 1000 }: StoneCountProps) => {
 }
 
 const Gold = ({ count = 1000 }: StoneCountProps) => {
+  const rigidBodiesGold = useRef<RapierRigidBody[]>(null)
   const { nodes, materials } = useGLTF('/models/rocks/gold1.glb') as unknown as StonesData
+
+  const instancesGold = useMemo(() => {
+    const instances: InstancedRigidBodyProps[] = []
+
+    goldRandomizer.forEach((props, i) => {
+      instances.push({
+        key: i,
+        position: [props.position[0], props.position[1], props.position[2]],
+        rotation: [props.rotation[0], props.rotation[1], props.rotation[2]],
+        scale: [10, 10, 10],
+      })
+    })
+
+    return instances
+  }, [])
+
   return (
-    <>
-      <Instances range={count} material={materials.Gold} geometry={nodes.Resource_Gold_1_1.geometry}>
-        {goldRandomizer.map((props, i) => (
-          <Gold1 key={i} {...props} scale={[10, 10, 10]} meshRef={nodes.Resource_Gold_1_1} />
-        ))}
-      </Instances>
-      <Instances range={count} material={materials.Stone} geometry={nodes.Resource_Gold_1_2.geometry}>
-        {goldRandomizer.map((props, i) => (
-          <Gold1 key={i} {...props} scale={[10, 10, 10]} meshRef={nodes.Resource_Gold_1_2} />
-        ))}
-      </Instances>
-    </>
+    <InstancedRigidBodies ref={rigidBodiesGold} instances={instancesGold} colliders="cuboid" type="fixed" onIntersectionEnter={(e) => console.log(e)}>
+      <instancedMesh castShadow args={[nodes.Resource_Gold_1_1.geometry, undefined, count]} count={count}>
+        <meshStandardMaterial attach="material" {...materials.Gold} />
+      </instancedMesh>
+      <instancedMesh castShadow args={[nodes.Resource_Gold_1_2.geometry, undefined, count]} count={count}>
+        <meshStandardMaterial attach="material" {...materials.Stone} />
+      </instancedMesh>
+    </InstancedRigidBodies>
   )
 }
 
 const Crystals = ({ count = 1000 }: StoneCountProps) => {
+  const rigidBodiesCrystals = useRef<RapierRigidBody[]>(null)
   const { nodes, materials } = useGLTF('/models/rocks/crystal1.glb') as unknown as StonesData
+
+  const instancesCrystals = useMemo(() => {
+    const instances: InstancedRigidBodyProps[] = []
+
+    crystalsRandomizer.forEach((props, i) => {
+      instances.push({
+        key: i,
+        position: [props.position[0], props.position[1], props.position[2]],
+        rotation: [props.rotation[0], props.rotation[1], props.rotation[2]],
+        scale: [1, 1, 1],
+      })
+    })
+
+    return instances
+  }, [])
+
   return (
-    <Instances range={count} material={materials['Material.004']} geometry={nodes.crystal_2.geometry}>
-      {crystalsRandomizer.map((props, i) => (
-        <Crystal1 key={i} {...props} scale={[1, 1, 1]} />
-      ))}
-    </Instances>
+    <InstancedRigidBodies ref={rigidBodiesCrystals} instances={instancesCrystals} colliders="cuboid" type="fixed" onIntersectionEnter={(e) => console.log(e)}>
+      <instancedMesh castShadow args={[nodes.crystal_2.geometry, undefined, count]} count={count}>
+        <meshStandardMaterial attach="material" {...materials['Material.004']} />
+      </instancedMesh>
+    </InstancedRigidBodies>
   )
-}
-
-const Gold1 = ({ ...props }) => {
-  const gold1 = useRef()
-  return <Instance ref={gold1} {...props} />
-}
-
-const Crystal1 = ({ ...props }) => {
-  const crystal1 = useRef()
-  return <Instance ref={crystal1} {...props} />
 }
 
 const Stones = () => {
   return (
     <>
       <Rocks count={1000} />
-      <Gold count={100} />
       <Crystals count={100} />
     </>
   )
